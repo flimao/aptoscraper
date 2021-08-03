988dae68cba61cba05841e50d077041de17360aa
Limão <dev@lmnice.me>
2021-08-03 18:09:59 -0300

    Definir campos da classe Item (W.I.P. #1)

--- aptoscraper/aptoscraper/items.py
+++ aptoscraper/aptoscraper/items.py
@@ -5,8 +5,32 @@
 
 import scrapy
 
-
-class AptoscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# ZAP Imóveis
+class ZAPItem(scrapy.Item):
+    id = scrapy.Field()
+    origem = scrapy.Field()
+    impulsao = scrapy.Field()
+    area = scrapy.Field()
+    desc = scrapy.Field()
+    nquartos = scrapy.Field()
+    nbanheiros = scrapy.Field()
+    nsuites = scrapy.Field()
+    nvagas = scrapy.Field()
+    preco = scrapy.Field()
+    despesa_mes = scrapy.Field()
+    despesa_ano = scrapy.Field()
+    iptu = scrapy.Field()
+    amenidades = scrapy.Field()
+    endereco_cep = scrapy.Field()
+    endereco_pais = scrapy.Field()
+    endereco_estado = scrapy.Field()
+    endereco_cidade = scrapy.Field()
+    endereco_bairro = scrapy.Field()
+    endereco_rua = scrapy.Field()
+    endereco_complemento = scrapy.Field()
+    pois = scrapy.Field()
+    contato_fones = scrapy.Field()
+    contato_whatsapp = scrapy.Field()
+    contato_nome = scrapy.Field()
+    link = scrapy.Field()
+    
